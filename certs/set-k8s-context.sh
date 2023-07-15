ee16658bf14e126c0a1424ce1a82a30a11156353
Syed-Mukthader <76219785+syedmukthader@users.noreply.github.com>
2023-07-15 18:21:41 +0100

    Update set-k8s-context.sh

--- certs/set-k8s-context.sh
+++ certs/set-k8s-context.sh
@@ -1,4 +1,4 @@
-kubectl config set-cluster kubernetes --server=$1 --certificate-authority=$3 --embed-certs=true 
-kubectl config set-credentials kubernetes-admin --token=$2
-kubectl config set-context kubernetes-admin@kubernetes --cluster kubernetes --user kubernetes-admin 
-kubectl config use-context kubernetes-admin@kubernetes
+kubectl config set-cluster kubernetes --server="$1" --certificate-authority="$3" --embed-certs=true
+kubectl config set-credentials kubernetes-admin --token="$2"
+kubectl config set-context kubernetes-admin --cluster=kubernetes --user=kubernetes-admin --namespace=default
+kubectl config use-context kubernetes-admin
